530851678920454f8d2cbbc2d45f55c7818ea41f
prafull raj <99156529+PrafullRaj@users.noreply.github.com>
2023-11-20 09:01:55 +0530

    Update Hamming code is one of the popular Error correction code in data link layer. Assume, It will add r  redundant bits to the original m data bits.  Write a program to find out the  r value for the given m  value.java

--- WEEK-2/Hamming code is one of the popular Error correction code in data link layer. Assume, It will add r  redundant bits to the original m data bits.  Write a program to find out the  r value for the given m  value.java
+++ WEEK-2/Hamming code is one of the popular Error correction code in data link layer. Assume, It will add r  redundant bits to the original m data bits.  Write a program to find out the  r value for the given m  value.java
@@ -1,58 +1,20 @@
 import java.util.Scanner;
-
-class Main
+public class week2_3 
 {
-  
-public static void main (String[]args)
-  {
-    
-Scanner sc = new Scanner (System.in);
-    
- 
-System.out.print ("Enter codeword 1: ");
-    
-String codeword1 = sc.nextLine ();
-    
- 
-System.out.print ("Enter codeword 2: ");
-    
-String codeword2 = sc.nextLine ();
-    
- 
-String xorRet = xorCs (codeword1, codeword2);
-    
-System.out.println (xorRet);
-  
-} 
- 
-public static String xorCs (String codeword1, String codeword2)
-  {
-    
-if (codeword1.length () != codeword2.length ())
-      {
-	
-return "-1";
-      
-}
-    else
-      {
-	
-StringBuilder xorRet = new StringBuilder ();
-	
-for (int i = 0; i < codeword1.length (); i++)
-	  {
-	    
-char bit1 = codeword1.charAt (i);
-	    
-char bit2 = codeword2.charAt (i);
-	    
-xorRet.append (bit1 == bit2 ? '0' : '1');
-	  
-} 
-return xorRet.toString ();
-      
-}
-  
-}
-
+    public static void main(String[] arg)
+    {
+        Scanner i1=new Scanner(System.in);
+        System.out.println("Enter m : ");
+        System.out.println("Number of redundant bits are : "+hammingR(i1.nextInt()));
+        i1.close();
+    }
+    static int hammingR(int m)
+    {
+        int r=0;
+        for(r=0;;r++)
+        {
+            if((Math.pow(2,r)-r)>=(m+1))
+                return r;
+        }
+    }
 }
