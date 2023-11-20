653f101bc3afaf156579d11049fa73975f1cbcd3
prafull raj <99156529+PrafullRaj@users.noreply.github.com>
2023-11-20 09:17:17 +0530

    Update Create a function to compute the XOR of two codewords.java

--- WEEK-2/Create a function to compute the XOR of two codewords.java
+++ WEEK-2/Create a function to compute the XOR of two codewords.java
@@ -3,7 +3,7 @@
 import java.util.Scanner;
 public class week2_2 
 {
-    public static void main(String[] ar)
+    public static void main(String[] arg)
     {
         Scanner i1=new Scanner(System.in);
         System.out.println("Enter codewords : ");
