c86c7c7d33e3eebcf900e7849aea0bc9d1a0cd82
prafull raj <99156529+PrafullRaj@users.noreply.github.com>
2023-11-20 09:33:01 +0530

    Update Hamming code is a popular error correction method. Write function to compute the following.java

--- WEEK 3/Hamming code is a popular error correction method. Write function to compute the following.java
+++ WEEK 3/Hamming code is a popular error correction method. Write function to compute the following.java
@@ -1,45 +1,31 @@
 import java.util.Scanner;
-
-public class Main {
-
-    public static String xor(String a, String b) {
-        StringBuilder result = new StringBuilder();
-        for (int i = 1; i < b.length(); i++) 
-            result.append((a.charAt(i) == b.charAt(i)) ? '0' : '1');
-        return result.toString();
+public class week3_2 
+{
+    public static void main(String[] ar)
+    {
+        Scanner i1=new Scanner(System.in);
+        System.out.println("Enter size of dataword : ");
+        int r=hammingR(i1.nextInt());
+        System.out.println("Number of redundant bits required for the given message are "+r);
+        bitPositions(r);
+        i1.close();
     }
-
-    public static String division(String dividend, String divisor) {
-        int pick = divisor.length();
-        String tmp = dividend.substring(0, pick);
-        for (int i = pick; i < dividend.length(); i++) 
-            tmp = (tmp.charAt(0) == '1') ? xor(divisor, tmp) + dividend.charAt(i) : xor("0".repeat(pick), tmp) + dividend.charAt(i);
-        return (tmp.charAt(0) == '1') ? xor(divisor, tmp) : xor("0".repeat(pick), tmp);
+    static int hammingR(int m)
+    {
+        // 2^r >= (m+r+1)
+        int r=0;
+        for(r=0;;r++)
+        {
+            if((Math.pow(2,r)-r) >= (m+1))
+                return r;
+        }
     }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        String data = getUserInput("Enter the dataword: ", sc);
-        String generator = getUserInput("Enter the CRC generator polynomial: ", sc);
-
-        String dividend = data + "0".repeat(generator.length() - 1);
-        String crc = division(dividend, generator);
-        String codeword = data + crc;
-
-        printInfo("Dataword", data);
-        printInfo("CRC Generator", generator);
-        printInfo("Dividend", dividend);
-        printInfo("CRC", crc);
-        printInfo("Codeword", codeword);
-    }
-
-    private static String getUserInput(String prompt, Scanner scanner) {
-        System.out.print(prompt);
-        return scanner.nextLine();
-    }
-
-    private static void printInfo(String label, String value) {
-        System.out.println(label + ": " + value);
+    static void bitPositions(int r)
+    {
+        System.out.println("Bit positions are : ");
+        for(int i=0;i<r;i++)
+        {
+            System.out.print((int)Math.pow(2,i)+" ");
+        }
     }
 }
